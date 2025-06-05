a4fc07f5f71e7a6a1ba7727a1df740028d82573d
Maurice Drew <drewm1987@gmail.com>
2025-06-05 18:31:33 -0500

    initial commit

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,15 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS tasks;
 
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT NOT NULL, 
+    password TEXT NOT NULL
+);
+
+CREATE TABLE tasks (
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL, 
+    done BOOLEAN NOT NULL,
+    user_id INTEGER NOT NULL
+)
